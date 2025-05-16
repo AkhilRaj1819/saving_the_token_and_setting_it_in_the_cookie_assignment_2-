d2d95664ece03c97594f41a714a6ac1b7c4b6b29
AkhilRaj1819 <vysyaraju.akhilraju.s89@kalvium.community>
2025-05-16 10:50:50 +0530

    done assignment

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const encrypt = (payload, secret) => {
-  // your code here and return token
+const jwt = require('jsonwebtoken');
+
+/**
+ * Creates a JWT token with an expiry time
+ * @param {Object} payload - Data to be encoded in the token
+ * @param {string} secret - Secret key for signing the token
+ * @param {string} expiresIn - Expiration time (default: '1h')
+ * @returns {string} JWT token
+ */
+const encrypt = (payload, secret, expiresIn = '1h') => {
+  // Create a JWT with payload, secret, and expiry time
+  const token = jwt.sign(payload, secret, { expiresIn });
+  return token;
 };
 
 module.exports = encrypt;
